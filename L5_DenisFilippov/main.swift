355cb05e5de70b77897bf3638688c429697c3e6d
Denix <denixx@live.ru>
2020-07-31 00:11:07 +0300

    Initial Commit

---
+++ L5_DenisFilippov/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  L5_DenisFilippov
+//
+//  Created by Denis Filippov on 31/7/20.
+//  Copyright © 2020 Denis Filippov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
